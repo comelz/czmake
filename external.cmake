eceef7dd05cdb14152191a880530286a96287b74
woggioni <woggioni@c434cad1-c900-0410-a15e-9cd15bb93181>
2018-03-08 17:29:48 +0000

    dependencies under lib directory
    git-svn-id: svn+ssh://svn.comelz.com/svn/packages/cmake_utils/trunk@91410 c434cad1-c900-0410-a15e-9cd15bb93181

--- external.cmake
+++ external.cmake
@@ -1,20 +1,26 @@
 
-FUNCTION(RESOLVE_EXTERNALS)
+FUNCTION(INIT_EXTERNALS)
+    set(options "")
+    set(oneValueArgs REPODIR)
+    set(multiValueArgs "")
+    cmake_parse_arguments(RESOLVE_EXTERNALS "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+
     PYFIND(INTERP VERSION 3)
     set(EXTERNALS_SCRIPT ${cmake_utils_SOURCE_DIR}/bin/external.py)
-    set(REPO_SOURCE_DIR ${cmake_utils_BINARY_DIR}/.repository/source)
-    set(REPO_BINARY_DIR ${cmake_utils_BINARY_DIR}/.repository/build)
+    if(NOT INIT_EXTERNALS_REPODIR)
+        set(INIT_EXTERNALS_REPODIR lib)
+    endif()
     set(ENV{PYTHONPATH} ${cmake_utils_SOURCE_DIR})
-    execute_process(COMMAND ${PYTHON3_EXE} ${EXTERNALS_SCRIPT} -s ${CMAKE_CURRENT_SOURCE_DIR} -r ${REPO_SOURCE_DIR}
+    execute_process(COMMAND ${PYTHON3_EXE} ${EXTERNALS_SCRIPT} -s ${CMAKE_CURRENT_SOURCE_DIR} -r ${INIT_EXTERNALS_REPODIR}
         RESULT_VARIABLE PROC_RES
         OUTPUT_VARIABLE PROC_STDOUT
         ERROR_VARIABLE  PROC_STDERR
-        WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
     )
     message("${PROC_STDOUT}")
     if(NOT PROC_RES EQUAL 0)
         message(FATAL_ERROR "${PROC_STDERR}")
     endif()
-    add_subdirectory(${REPO_SOURCE_DIR} ${REPO_BINARY_DIR})
+    add_subdirectory(${INIT_EXTERNALS_REPODIR})
 ENDFUNCTION()
 
