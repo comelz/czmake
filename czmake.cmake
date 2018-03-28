0961b12ba37021970c45cffaa375f158dee19789
Walter Oggioni <woggioni@comelz.com>
2018-03-28 18:31:10 +0200

    fixed module installation paths

---
+++ czmake.cmake
@@ -0,0 +1,7 @@
+
+set(CZMAKE_ROOT_DIR ${CMAKE_ROOT}/Modules/czmake CACHE PATH "")
+include(czmake/misc.cmake)
+include(czmake/sip.cmake)
+include(czmake/subversion.cmake)
+include(czmake/library.cmake)
+include(czmake/dependencies.cmake)
