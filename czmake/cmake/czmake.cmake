c080a5196d6b8d52aca108d62e20f4de0da2a28f
Walter Oggioni <woggioni@comelz.com>
2018-05-10 10:53:03 +0200

    semplificazion to only act as a CMake wrapper

--- czmake/cmake/czmake.cmake
+++
@@ -1,4 +0,0 @@
-include(${CMAKE_CURRENT_LIST_DIR}/private/misc.cmake)
-include(${CMAKE_CURRENT_LIST_DIR}/private/sip.cmake)
-include(${CMAKE_CURRENT_LIST_DIR}/private/subversion.cmake)
-include(${CMAKE_CURRENT_LIST_DIR}/private/library.cmake)
